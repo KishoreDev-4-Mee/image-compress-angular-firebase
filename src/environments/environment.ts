724ee76da5dd60008591e8b388fe46c4147fd509
Kishore <77486234+KishoreDev-4-Mee@users.noreply.github.com>
2024-03-20 19:55:41 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,22 @@
 export const environment = {
   firebase: {
-    projectId: 'imagecompress-a31bd',
-    appId: '1:1051949330327:web:ec0a5fd3a7971561fc1ed2',
-    storageBucket: 'imagecompress-a31bd.appspot.com',
-    locationId: 'asia-south1',
-    apiKey: 'AIzaSyCacnRvr_9l34Dop54sBjsvAEVSh7ZYcpM',
-    authDomain: 'imagecompress-a31bd.firebaseapp.com',
-    messagingSenderId: '1051949330327',
-    measurementId: 'G-X8T23HLPXT',
+    projectId: 'xxxxxxxxxxxxx',
+    appId: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+    storageBucket: 'xxxxxxxxxxxxxxxxxxxxxx',
+    locationId: 'xxxxxxxxxxxxxxx',
+    apiKey: 'xxxxxxxxxxxxxxxxxxxxx',
+    authDomain: 'xxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    messagingSenderId: 'xxxxxxxxxxxxxxxxxxxxxxxx',
+    measurementId: 'xxxxxxxxxx',
   },
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCacnRvr_9l34Dop54sBjsvAEVSh7ZYcpM",
-    authDomain: "imagecompress-a31bd.firebaseapp.com",
-    projectId: "imagecompress-a31bd",
-    storageBucket: "imagecompress-a31bd.appspot.com",
-    messagingSenderId: "1051949330327",
-    appId: "1:1051949330327:web:ec0a5fd3a7971561fc1ed2",
-    measurementId: "G-X8T23HLPXT"
+    apiKey: "xxxxxxxxxxxxxxxxxxxxxx",
+    authDomain: "xxxxxxxxxxxxxxxxxxxx",
+    projectId: "xxxxxxxxxxxxxxxx",
+    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxx",
+    messagingSenderId: "xxxxxxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxx",
+    measurementId: "xxxxxxxxxxxxxxxxxxxxx"
   }
 };
